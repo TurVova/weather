b3cf160a7d9aad6342778d6fef8b91c64e62f3ba
turvova <1989turvova@gmail.com>
2020-02-10 17:01:44 +0200

    Create endpoints for new urls

--- api_weather/urls.py
+++ api_weather/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from api_weather import views
 
-
 urlpatterns = [
-    path('syncWeather/', views.receive_weather_data),
-    path('currentWeather/', views.get_weather_data),
+    path('syncWeather/', views.ReceiveDataWeatherRetrieveAPIView.as_view()),
+    path('currentWeather/', views.GetDataWeatherRetrieveAPIView.as_view()),
 ]
